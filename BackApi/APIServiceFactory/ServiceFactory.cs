389364f28dd9bf2cafd5406d02e4dc09bc4fa44f
nushey <zeballosnahuelfd@gmail.com>
2025-11-10 18:53:15 -0300

    feat: add service registration in ServiceFactory

--- BackApi/APIServiceFactory/ServiceFactory.cs
+++ BackApi/APIServiceFactory/ServiceFactory.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IServices;
+using Services;
 
 namespace APIServiceFactory
 {
     public static class ServiceFactory
     {
-        public static void AddServices(this IServiceCollection serviceCollection){ }
+        public static void AddServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IReflectionService, ReflectionService>();
+        }
     }
 }
