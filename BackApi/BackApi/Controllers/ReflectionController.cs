bc33f5c4f5770b367e83062489e9575834d5e2b7
nushey <zeballosnahuelfd@gmail.com>
2025-11-10 18:56:30 -0300

    feat: implement GetImporters endpoint in ReflectionController

--- BackApi/BackApi/Controllers/ReflectionController.cs
+++ BackApi/BackApi/Controllers/ReflectionController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IServices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ReflectionController : ControllerBase
+    public class ReflectionController(IReflectionService reflectionService) : ControllerBase
     {
+        [HttpGet("importers")]
+        public IActionResult GetImporters()
+        {
+            try
+            {
+                var assemblyNames = reflectionService.GetAssembliesWithImporter();
+                return Ok(assemblyNames);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An internal server error occurred.");
+            }
+        }
     }
 }
