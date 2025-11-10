9ec56ef5dbfa42a9941c47c4b29dde421f858fe7
nushey <zeballosnahuelfd@gmail.com>
2025-11-10 19:18:59 -0300

    feat: enhance ReflectionService to load assemblies and check for Importer implementations

--- BackApi/Services/ReflectionService.cs
+++ BackApi/Services/ReflectionService.cs
@@ -1,11 +1,53 @@
-﻿using IServices;
+﻿using System.Reflection;
+using IImporter;
+using IServices;
 
 namespace Services;
 
-public class ReflectionService : IReflectionService
+public class ReflectionService(string reflectionFolderPath) : IReflectionService
 {
-    public IEnumerable<string> GetAssembliesWithImporter()
-    {
-        return Enumerable.Empty<string>();
+        private readonly string _reflectionFolderPath = reflectionFolderPath ?? throw new ArgumentNullException(nameof(reflectionFolderPath));
+
+        public IEnumerable<string> GetAssembliesWithImporter()
+        {
+            if (!Directory.Exists(_reflectionFolderPath))
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            var dllFiles = Directory.GetFiles(_reflectionFolderPath, "*.dll");
+            var assembliesFound = new List<string>();
+
+            foreach (var file in dllFiles)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(file);
+
+                    if (AssemblyContainsImporter(assembly))
+                    {
+                        assembliesFound.Add(Path.GetFileName(file));
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+
+            return assembliesFound;
+        }
+
+        private bool AssemblyContainsImporter(Assembly assembly)
+        {
+            foreach (Type type in assembly.GetExportedTypes())
+            {
+                if (typeof(ImporterInterface).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
-}
